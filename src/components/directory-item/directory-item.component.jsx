c043e5bf4c72fece3a22aa49209d76ad72a78eb6
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-03-23 23:19:29 +0400

    modified home pages structure and added links to navigate some given places

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from 'react-router-dom'
+
 import {
     BackgroundImage,
     Body,
     DirctoryItemContainer,
 } from './directory-item.styles'
 
-const DirctoryItem = ({ imageUrl, title }) => {
+const DirctoryItem = ({ imageUrl, title, route }) => {
+    const navigate = useNavigate()
+
+    const onNavigateHandler = () => navigate(route)
+
     return (
-        <DirctoryItemContainer>
+        <DirctoryItemContainer onClick={onNavigateHandler}>
             <BackgroundImage imageUrl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
