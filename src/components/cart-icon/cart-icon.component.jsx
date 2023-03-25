5d1805c6bca2e7a0dc7338540832487c4d9cfac4
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-03-25 18:19:36 +0400

    changed states by reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,8 +5,8 @@ import { CartContext } from '../../contexts/cart.context'
 import { CartIconContainer, ItemCount, ShoppingIcon } from './cart-icon.styles'
 
 const CartIcon = () => {
-    const { setIsCartOpen, cartCount } = useContext(CartContext)
-    const toggleIsCartOpen = () => setIsCartOpen((prev) => !prev)
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
+    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
 
     return (
         <CartIconContainer onClick={toggleIsCartOpen}>
