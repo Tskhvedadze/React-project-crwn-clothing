a9adb7f2f653f27278dc6772ad994b74dbc48960
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-03-26 17:28:11 +0400

    transfer category context in to the redux store and cut to the seperated files also modified firebase utils

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+
+import { useDispatch } from 'react-redux'
 
 import CategoriesPreview from '../categories-preview/categories-preview.component'
 import Category from '../category/category.component'
 
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
+import { setcategories } from '../../store/categories/category.action'
+
 const Shop = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoriesArray = await getCategoriesAndDocuments()
+            dispatch(setcategories(categoriesArray))
+        }
+        getCategoriesMap()
+    }, [dispatch])
+
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
