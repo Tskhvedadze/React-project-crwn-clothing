6b4fcc5254d80931a87dabaaa8403d36dd83ceb0
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-03-28 16:14:39 +0400

    added user redux-saga

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 
-import {
-    createUserDocumentFromAuth,
-    onAuthStateChangedListener,
-} from './utils/firebase/firebase.utils'
-
-import { setCurrentUser } from './store/user/user.action'
-
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './routes/home/home.component'
@@ -16,18 +9,13 @@ import Authentication from './routes/authentication/authentication.components'
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
 
+import { checkUserSession } from './store/user/user.action'
+
 const App = () => {
     const dispatch = useDispatch()
 
     useEffect(() => {
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            if (user) {
-                createUserDocumentFromAuth(user)
-            }
-            dispatch(setCurrentUser(user))
-        })
-
-        return unsubscribe
+        dispatch(checkUserSession())
     }, [dispatch])
 
     return (
