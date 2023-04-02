176eb40a68557d1c7fd0209d15f901c31ad1f13b
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-04-02 14:35:56 +0400

    changed button component to the ts format

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import { FC, ButtonHTMLAttributes } from 'react'
+
 import {
     BaseButton,
     GoogleSignInButton,
@@ -5,20 +8,30 @@ import {
     ButtonSpinner,
 } from './button.styles'
 
-export const BUTTON_TYPE_CLASSES = {
-    base: 'base',
-    google: 'google',
-    inverted: 'inverted',
+export enum BUTTON_TYPE_CLASSES {
+    base = 'base',
+    google = 'google',
+    inverted = 'inverted',
 }
 
-const getButton = (buttonTypes = BUTTON_TYPE_CLASSES.base) =>
+const getButton = (buttonTypes = BUTTON_TYPE_CLASSES.base): typeof BaseButton =>
     ({
         [BUTTON_TYPE_CLASSES.base]: BaseButton,
         [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
         [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
     }[buttonTypes])
 
-const Button = ({ children, buttonTypes, isLoading, ...otherProps }) => {
+export type ButtonProps = {
+    buttonTypes?: BUTTON_TYPE_CLASSES
+    isLoading?: boolean
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button: FC<ButtonProps> = ({
+    children,
+    buttonTypes,
+    isLoading,
+    ...otherProps
+}) => {
     const CustomButton = getButton(buttonTypes)
     return (
         <CustomButton disabled={isLoading} {...otherProps}>
